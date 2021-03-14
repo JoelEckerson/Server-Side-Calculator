1dd67f39e6d3319c65de2ce8f2047e9f5d6b0f8d
Joel Eckerson <joelaeckerson@gmail.com>
2021-03-14 14:54:19 -0500

    fixing answer

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,14 +3,16 @@ $ ( document ).ready( onReady );
 let equation = {
     firstNumber: 0,
     operator: '',
-    secondNumber: 0
+    secondNumber: 0,
+    result: ''
 };
 
 function onReady(){
     console.log('JQ');
     $( '#equalButton' ).on( 'click', onEqualButton );
     $( '.operator' ).on('click', onOperatorButton );
-
+    $( '#clearButton').on('click', onClearButton );
+    getCalculationResult();
 } // end onReady
 
 function onEqualButton(){
@@ -32,7 +34,10 @@ function onEqualButton(){
         // catch an error
         alert( 'error performing calculation' );
         console.log( err );
-    } )
+    })// end post
+    let response = getCalculationResult();
+    updateResults( response );
+
 } // end onEqualButton
 
 function onOperatorButton(){
@@ -42,3 +47,45 @@ function onOperatorButton(){
     equation.operator = $( this ).text();
 } // end onOperatorButton
 
+// create function to GET info from server
+function getCalculationResult( ){
+    $.ajax({
+        type: 'GET',
+        url: '/calculation'
+    }).then( function( response ){
+        // console log the response
+        console.log( 'back from GET:', response );
+        // create variable for history id
+        let el = $( '#history' );
+        // empty results
+        el.empty();
+        // loop through history from server to append to the DOM
+        for (let i = 0; i < response.length; i++) {
+            // append to DOM
+            el.append(`<li>${response[i].firstNumber} ${response[i].operator} ${response[i].secondNumber} = ${response[i].result}</li>`);
+        }
+        return response;
+    }).catch( function( err ){
+        // alert if there are any issues
+        alert( 'error getting calculationResults from server ' + err );
+        console.log( err );
+    })// end get
+} // end getCalculationResults
+
+// create function for clear button
+function onClearButton( ){
+    // console log
+    console.log( 'in onClearButton' );
+    // empty the inputs
+    $('#firstInput').val( ' ' );
+    $('#secondInput').val( ' ' );
+}// end onClearButton
+
+function updateResults( response ){
+    // create variable for results id
+    let element = $( '#result' );
+    // empty
+    element.empty();
+    // append the answer to the DOM
+    element.append(`${response[response.length - 1].result}`);
+}
--- server/server.js
+++ server/server.js
@@ -7,7 +7,8 @@ app.use( express.static( 'server/public' ) );
 app.use( bodyParser.urlencoded( { extended: true } ) );
 // globals
 const port = 5000;
-let calcuationResult = 0;
+
+// create array to hold the history
 let history = [];
 
 // spin up server
@@ -15,32 +16,63 @@ app.listen( port, ()=>{
     console.log( 'server is up on:', port );
 })
 
+// create post to put information on server
 app.post( '/calculation', ( req, res ) =>{
+    // console log
     console.log( 'in /calculation POST:', req.body );
+    // creating variable to send the status
     let status = performCalculation( req.body );
+    // sending status
     res.sendStatus( status );
+    // console log the status
     console.log ( status );
+    // call a function to get history
+    onHistory( req.body );
 } )
 
+// create get to send information bakc
+app.get( '/calculation', (req, res ) =>{
+    // send results
+    res.send( history );
+    // console log
+    console.log( 'in /calculation GET', history );
+} )
+
+// create function to do the equations
 function performCalculation( equation ){
+    // make variable to send back the 'OK'
     let returnStatus = 200;
+    // create a switch to do the equation based on the button pushed
     switch( equation.operator ){
+        // the case for addition
         case '+': 
-            calcuationResult = Number(firstNumber) + Number(secondNumber);
+            equation.result = Number(equation.firstNumber) + Number(equation.secondNumber);
         break;
+        // the case for subtraction
         case '-': 
-            calcuationResult = Number(firstNumber) - Number(secondNumber);
+            equation.result = Number(equation.firstNumber) - Number(equation.secondNumber);
         break;
+        // the case for multiplication
         case '*': 
-            calcuationResult = Number(firstNumber) * Number(secondNumber);
+            equation.result = Number(equation.firstNumber) * Number(equation.secondNumber);
         break;
+        // the case for division
         case '/': 
-            calcuationResult = Number(firstNumber) / Number(secondNumber);
+            equation.result = Number(equation.firstNumber) / Number(equation.secondNumber);
         break;
+        // create a default if it doesn't work
         default:
             console.log( 'no operator found');
             returnStatus = 400;
     }
+    // return either an 'OK' or 'ERROR'
     return returnStatus;
 }
+
+// create a function to push the history into the global array
+function onHistory( equation ){
+    history.push( equation );
+}
+
+
 
